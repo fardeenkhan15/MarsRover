d07a44cc5ed769b3326e64466b527c6ce6ef40d1
fardeenkhan15 <mdfardeen232002@gmail.com>
2024-07-06 15:15:09 +0530

    fix

--- src/ScanCommand.java
+++
@@ -1,8 +0,0 @@
-// public class ScanCommand implements Command {
-//     @Override
-//     public void execute(Rover rover, Grid grid) {
-//         String scanResult = rover.scanSurroundings(grid);
-//         System.out.println(scanResult);
-//         Logger.log("Scan performed at " + rover.getPosition().getX() + "," + rover.getPosition().getY());
-//     }
-// }
